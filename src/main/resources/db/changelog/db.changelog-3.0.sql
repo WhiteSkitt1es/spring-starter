51cc8a30e9cb1a55ac2af59edbea73a7988e313a
Pavel Artem'yev <129493668+WhiteSkitt1es@users.noreply.github.com>
2024-01-25 12:25:39 +0300

    Delete src/main/resources directory
    Spring Complete!

--- src/main/resources/db/changelog/db.changelog-3.0.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset example:1
-ALTER TABLE users
-    ADD COLUMN image VARCHAR(64);
-
---changeset example:2
-ALTER TABLE users_aud
-    ADD COLUMN image VARCHAR(64);
--- src/main/resources/db/changelog/db.changelog-4.0.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset example:1
-ALTER TABLE users
-    ADD COLUMN password VARCHAR(128) DEFAULT '{noop}123';
-
---changeset example:2
-ALTER TABLE users_aud
-    ADD COLUMN password VARCHAR(64);
